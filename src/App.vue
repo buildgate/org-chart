582e6a80861cf9910b051f01f53d3c594a230082
liangzhibin <buildgate@qq.com>
2025-03-10 16:05:40 +0800

    fix

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="chart-wrapper">
+    <!-- <svg
+      class="svg-content"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    ></svg> -->
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.chart-wrapper {
+  width: 100%;
+  height: 100%;
+}
+.svg-content {
+  height: 100%;
+  width: 100%;
+}
+</style>
